f81ac66815998e9675d9b2ea5e0464b8c005da64
fahimtakin <fahim.tanzil@hotmail.com>
2022-08-27 18:10:35 +0600

    Palindrome Linked List updated

--- Python/234-PalindromeLinkedList.py
+++ Python/234-PalindromeLinkedList.py
@@ -6,21 +6,21 @@ def isPalindrome(self, head: Optional[ListNode]) -> bool:
         while fastPtr is not None and fastPtr.next is not None:
             fastPtr = fastPtr.next.next # fast pointer increments twice as slow pointer
             slowPtr = slowPtr.next # increments +1
+        
         prevPtr = None   
         #reverse second half
         while slowPtr is not None:
             tmp = slowPtr.next
-            slow.next = prevPtr
-            prev = slowPtr
+            slowPtr.next = prevPtr
+            prevPtr = slowPtr
             slowPtr = tmp
             
         #check palindrome
         # two pointers coming from two ends and checking for each value
-        leftPtr, rightPtr = head, prev
+        leftPtr, rightPtr = head, prevPtr
         while rightPtr is not None: 
             if leftPtr.val != rightPtr.val:
                 return False
             leftPtr = leftPtr.next
-            rightPtr = rightPtr.next
-            
+            rightPtr = rightPtr.next    
         return True
