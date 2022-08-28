5521bbfb0353e3ed9ec7cc0024957eb3eb806404
fahimtakin <fahim.tanzil@hotmail.com>
2022-08-28 10:56:18 +0600

    Longest Common Prefix added

---
+++ Python/14-LongestCommonPrefix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = "" # initially no common
+        for i in range(len(strs[0])): # looping through all the letters of the first element
+            for s in strs: # looping through all the strings
+                if i == len(s) or s[i] != strs[0][i]: # checking if index isn't out of bound or index of arbirary string is not equal to the strings
+                    return res
+            res += strs[0][i]
+        return 
