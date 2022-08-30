c45355bbee64672489f62c3ff89f3dd38ebd6544
fahimtakin <fahim.tanzil@hotmail.com>
2022-08-31 00:24:19 +0600

    Path Sum updated

--- Python/112-PathSum.py
+++ Python/112-PathSum.py
@@ -1,10 +1,12 @@
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         def dfs(node, currentSum):
-            if not node: # if no node is there, return false
-                return false
+            if not node:  # if no node is there, return false
+                return False
             currentSum += node.val
-            if not node.left and not node.right: # if it's a leaf node
+            if not node.left and not node.right:  # if it's a leaf node
                 return currentSum == targetSum
-            return (dfs(node.left, currentSum) or dfs(node.right, currentSum)) # if not leaf node then call left and right subtree recursively
-        return dfs(root, 0) # call the dfs function
+            return (dfs(node.left, currentSum) or dfs(node.right,
+                                                      currentSum))  # if not leaf node then call left and right subtree recursively
+
+        return dfs(root, 0)  # call the dfs function
