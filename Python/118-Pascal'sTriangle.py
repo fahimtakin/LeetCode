3ebfa51a07c7ddf1eb18bb18750c8f9ca940d138
fahimtakin <fahim.tanzil@hotmail.com>
2022-08-31 12:43:40 +0600

    Pascal's Triangle updated

--- Python/118-Pascal'sTriangle.py
+++ Python/118-Pascal'sTriangle.py
@@ -1,12 +1,12 @@
 class Solution:
-        def generate(self, numRows: int) -> List[List[int]]:
-            res = [[1]] # base case
-            
-            for i in range(numRows - 1): # How many rows
-                temp = [0] + res[-1] + [0] # building a temporary array
-                row = []
-                for j in range (len[res-1] + 1): # build next row
-                    row.append(temp[j] + temp[j+1]) # add the two values
-                res.append(row)
-                
-            return res
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]  # base case
+
+        for i in range(numRows - 1):  # How many rows
+            temp = [0] + res[-1] + [0]  # building a temporary array
+            row = []
+            for j in range(len(res[-1]) + 1):  # build next row
+                row.append(temp[j] + temp[j + 1])  # add the two values
+            res.append(row)
+
+        return res
