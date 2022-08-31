71a4cc85216ba56303ad561656c601e06173911a
fahimtakin <fahim.tanzil@hotmail.com>
2022-08-31 15:32:27 +0600

    Remove Duplicates from Sorted List added

--- Python/83-RemoveDuplicatesFromSortedList.py
+++ Python/83-RemoveDuplicatesFromSortedList.py
@@ -5,13 +5,12 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curPtr = head #curPtr pointing at head
-        
-        while curPtr: # while curPtr is not null
-            while curPtr.next and curPtr.next.val == curPtr.val # while curPtr.next is not null and curPtr.next value is equal to curPtr value
-                curPtr.next=curPtr.next.next # curPtr.next is changed
-                
-            curPtr = curPtr.next 
-            
+        curPtr = head  # curPtr pointing at head
+
+        while curPtr:  # while curPtr is not null
+            while curPtr.next and curPtr.next.val == curPtr.val:  # while curPtr.next is not null and curPtr.next value is equal to curPtr value
+                curPtr.next = curPtr.next.next  # curPtr.next is changed
+
+            curPtr = curPtr.next
+
         return head
-        
